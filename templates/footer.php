ee1518421a763d7553bd250aac1f26fe872bd0db
Buldin Alexandr <buldin.alieksandr@gmail.com>
2022-03-07 13:37:32 +0300

    add modal window & some fix

--- templates/footer.php
+++ templates/footer.php
@@ -2,8 +2,46 @@
 session_start();
 ?>
 
+<div class="modal fade" id="modal-cart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-xl">
+              <div class="modal-content">
+                <div class="modal-header text-white" style="background-color: #282626;">
+                  <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <table class="table">
+                    <tbody>
+                      <tr>
+                        <td><img src="img/product/1.jpg" alt=""></td>
+                        <td><a href="#">Washer</a></td>
+                        <td>$499.99</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td><img src="img/product/2.jpg" alt=""></td>
+                        <td><a href="#">Toaster</a></td>
+                        <td>$289.89</td>
+                        <td>1</td>
+                      </tr>
+                      <tr>
+                        <td><img src="img/product/3.jpg" alt=""></td>
+                        <td><a href="#">Mixer</a></td>
+                        <td>$150.90</td>
+                        <td>1</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
 <footer>
-  <section class="footer">
     <div class="container">
       <div class="row">
         <div class="col-md-3 col-6">
@@ -38,7 +76,6 @@ session_start();
         </div>
       </div>
     </div>
-  </section>
 </footer>
 
 <script src="js/bootstrap.js"></script>
