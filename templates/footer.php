6a433ec6e1426ab93975f563aadbe71110cb4c9b
Buldin Alexandr <buldin.alieksandr@gmail.com>
2022-03-10 21:51:07 +0300

    implemented adding a product to the cart

--- templates/footer.php
+++ templates/footer.php
@@ -3,81 +3,111 @@ session_start();
 ?>
 
 <div class="modal fade" id="modal-cart" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-xl">
-              <div class="modal-content">
-                <div class="modal-header text-white" style="background-color: #282626;">
-                  <h5 class="modal-title" id="exampleModalLabel">Cart</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                  <table class="table">
-                    <tbody>
-                      <tr>
-                        <td><img src="img/product/1.jpg" alt=""></td>
-                        <td><a href="#">Washer</a></td>
-                        <td>$499.99</td>
-                        <td>1</td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/product/2.jpg" alt=""></td>
-                        <td><a href="#">Toaster</a></td>
-                        <td>$289.89</td>
-                        <td>1</td>
-                      </tr>
-                      <tr>
-                        <td><img src="img/product/3.jpg" alt=""></td>
-                        <td><a href="#">Mixer</a></td>
-                        <td>$150.90</td>
-                        <td>1</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-                <div class="modal-footer">
-                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                  <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-              </div>
-            </div>
-          </div>
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header text-white" style="background-color: #282626;">
+        <h5 class="modal-title" id="exampleModalLabel">Корзина</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <table class="table">
+          <thead>
+            <td></td>
+            <td>Наименование</td>
+            <td>Стоимость</td>
+            <td>Колличество</td>
+            <td>Итого</td>
+          </thead>
+          <tbody>
+            <?php
+            $sql_m = $link->query("SELECT * FROM `products`");
+            $Sum = 0;
+            $add_product = $_SESSION['add_product'];
+
+            if (isset($add_product)) {
+              foreach ($add_product as $key => $value) {
+                $a = $key;
+                $kol =  $_SESSION['add_product'][$a];
+                $good_m = [];
+                foreach ($sql_m as $product_m) {
+                  if ($product_m['id'] == $a) {
+                    $good_m = $product_m;
+                    break;
+                  }
+                }
+            ?>
+                <tr>
+                  <td><img width="50px" src="<?php echo $good_m['imgs']; ?>" /></td>
+                  <td><?php echo $good_m['name']; ?></td>
+                  <td><input type="button" value="-" id="button_minus">
+                    <input type="number" step="1" min="1" max="10" id="num_count" name="quantity" value="<?php echo $kol; ?>" title="Qty">
+                    <input type="button" value="+" id="button_plus">
+                  </td>
+
+
+                  <td><?php echo $good_m['price'] . 'р'; ?></td>
+                  <td><?php echo $kol * $good_m['price'] . 'р'; ?></td>
+                  <td align="right" colspan="5"><b> <a href="#">Удалить</a></b></td>
+
+                </tr>
+            <?php
+                $Sum += $kol * $good_m['price'];
+              }
+            }
+            // Выводим итоговую сумму заказа
+            ?>
+            <tr>
+              <td align="right" colspan="5"><b> <?php echo 'Всего: ' . $Sum ?></b></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="modal-footer">
+      <b> <a href="add_basket.php">Оформить заказ</a></b>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3 col-6">
-          <h4>Information</h4>
-          <ul class="list-unstyled">
-            <li><a href="index.php?page=index">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Payment and delivery</a></li>
-            <li><a href="#">Contacts</a></li>
-          </ul>
-        </div>
-        <div class="col-md-3 col-6">
-          <h4>Working Time</h4>
-          <ul class="list-unstyled">
-            <li>st Pushkina, d Kolotushkina</li>
-            <li>Mon - Fri: 8am - 5pm</li>
-          </ul>
-        </div>
-        <div class="col-md-3 col-6">
-          <h4>Contacts</h4>
-          <ul class="list-unstyled">
-            <li><a href="tel:89527735225">+7(934) 123-58-55</a></li>
-          </ul>
-        </div>
-        <div class="col-md-3 col-6">
-          <h4>Messangers</h4>
-          <div class="footer-icons">
-            <a href="#"><i class="fab fa-vk"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-tiktok"></i></a>
-          </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-6">
+        <h4>Information</h4>
+        <ul class="list-unstyled">
+          <li><a href="index.php?page=index">Home</a></li>
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Payment and delivery</a></li>
+          <li><a href="#">Contacts</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <h4>Working Time</h4>
+        <ul class="list-unstyled">
+          <li>st Pushkina, d Kolotushkina</li>
+          <li>Mon - Fri: 8am - 5pm</li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <h4>Contacts</h4>
+        <ul class="list-unstyled">
+          <li><a href="tel:89527735225">+7(934) 123-58-55</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3 col-6">
+        <h4>Messangers</h4>
+        <div class="footer-icons">
+          <a href="#"><i class="fab fa-vk"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-tiktok"></i></a>
         </div>
       </div>
     </div>
+  </div>
 </footer>
 
 <script src="js/bootstrap.js"></script>
 </body>
+
 </html>
--- templates/tovarCart.php
+++ templates/tovarCart.php
@@ -3,15 +3,39 @@
   <div class="tovar d-flex-column">
     <div class="header"><?php echo $good['name']; ?></div>
     <div class="body">
-        <?php echo $good['desc']; ?>
+      <?php echo $good['desc']; ?>
       <div class="product-bottom-details d-flex justify-content-between">
         <div class="product-price">
-          <small>$300</small> <?php echo $good['price'].' $'; ?>
+          <small>$300</small> <?php echo $good['price'] . ' $'; ?>
         </div>
-        <div class="product-links">
-          <a href="#"><i class="fas fa-shopping-basket"></i><span>Add to cart</span></a>
-          <a href="#"><i class="fas fa-heart"></i></a>
-        </div>
+
+        <form id="form1" name="form1" action="add_cart.php" method="post">
+          <div class="input-group quantity_goods">
+            <input type="button" value="-" id="button_minus">
+            <input onchange="location=value" type="number" step="1" min="1" max="10" id="num_count" name="quantity" value="1" title="Qty">
+            <input type="button" value="+" id="button_plus">
+          </div>
+          <input type="hidden" name="product_id" value="<?php echo $good['id'] ?>" />
+          <input class='add_to_cart' type="submit" value="В корзину" name="submit">
+        </form>
+
+        <script>
+          var numCount = document.getElementById('num_count');
+          var plusBtn = document.getElementById('button_plus');
+          var minusBtn = document.getElementById('button_minus');
+          plusBtn.onclick = function() {
+            var qty = parseInt(numCount.value);
+            qty = qty + 1;
+            numCount.value = qty;
+          }
+          minusBtn.onclick = function() {
+            var qty = parseInt(numCount.value);
+            if (qty > 1) {
+              qty = qty - 1;
+            }
+            numCount.value = qty;
+          }
+        </script>
       </div>
     </div>
   </div>
